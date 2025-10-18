f41e933bd923b6c6fdb248b5714f1a38fb51db59
Rashin2002 <rashinprashastha2002@gmail.com>
2025-10-18 17:35:58 +0530

    Start with .Net 8 and Start the MVC

---
+++ TMS NET8/TMS NET8/Database/DBConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace TMS_NET8.Database
+{
+    public static class DBConnection
+    {
+        public static SqlConnection CreateConnection()
+        {
+            string connectionString = "Data Source=Rashinpc;Initial Catalog=TenderDB;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            return con;
+        }
+    }
+}
+
+//xmlns: local = "clr-namespace:TMS_NET8"
